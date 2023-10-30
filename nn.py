b55f1ad082a9884de3ae2db7372959d782648ebe
Smit Shah <smitshah1999@gmail.com>
2023-10-30 03:09:29 -0500

    implement forward layer skeleton for a  point

--- nn.py
+++ nn.py
@@ -36,13 +36,25 @@ def visualize(index: int):
     plt.show()
 
 
-def visualize_distribution(y_value):
-    y_value = y_value.ravel()
-    x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    plt.xlabel('label')
-    plt.ylabel('count')
-    plt.bar(x_value, y_value, 0.7, color='g')
-    plt.show()
+def check_count_of_each_label():
+    y_value = np.zeros((1, 10))
+    for i in range(10):
+        print("Occurence of ", i, "=", np.count_nonzero(train_labels == i))
+        y_value[0, i-1] = np.count_nonzero(train_labels == i)
+        y_value = y_value.ravel()
+        x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        plt.xlabel('label')
+        plt.ylabel('count')
+        plt.bar(x_value, y_value, 0.7, color='g')
+        plt.show()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 
 if __name__ == "__main__":
@@ -65,15 +77,24 @@ if __name__ == "__main__":
         test = pd.read_csv("./data/MNIST/raw/mnist_test.csv")
     train_labels = np.array(train.loc[:, 'label'])
     train_data = np.array(train.loc[:, train.columns != 'label'])
-    print(train.shape, test.shape)
     # visualize(0)
+    # check_count_of_each_label(train_labels)
 
-    print("Train data")
-    sumi = 0
-    y_value = np.zeros((1, 10))
-    print(y_value)
-    for i in range(10):
-        print("Occurence of ", i, "=", np.count_nonzero(train_labels == i))
-        y_value[0, i-1] = np.count_nonzero(train_labels == i)
-    
-    # visualize_distribution(y_value)
+    d = train_data.shape[1]
+    d1 = 300
+    # Shape of W1 is given by d1 * d where d1 is 300 and d is given by 784
+    W1 = np.zeros((d1, d))
+
+    # print(W1.shape)
+    x1 = train_data[0]
+    # print(x1.shape, x1)
+    z1 = np.dot(W1, x1)
+    # print(z1.shape, z1)
+    a1 = sigmoid(z1)
+    print('After sigmmoid activation shape is', a1.shape)
+
+    W2 = np.zeros((10, d1))
+    z2 = np.dot(W2, a1)
+    # print(z2, z2.shape)
+    y_pred = softmax(z2)
+    print(y_pred.shape, y_pred)
